f8be2d45a4ce67c281f3556dfcb4bb2a9cc75687
Jmyjenkins <jmyjenkins10@gmail.com>
2025-08-05 19:23:08 +0000

    ¨testimonial¨

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
-console.log("hello world")
+// console.log("hello world")
 
-let chicken=123
-console.log(chicken);
-
-
-console.log
-console,log(10+"eggs");
-console.log(10 + 8 + "eggs");
-
-//both Conditions Need To Be True 
-console.log()
-
-//Only one condition must be true
-console.log(10>3)||(5>3);
-
-//If the statement is logical or not 
+// let chicken=123
+// console.log(chicken);
 
 
+// console.log
+// console,log(10+"eggs");
+// console.log(10 + 8 + "eggs");
 
-const hour =new Date().get hours
-let greeting ="";
-if (hour > 12) {
-} greeting = "goodmorning!";
-}else if (hour < 18) {
-greeting = "good afternoon!";
-} else {
-    greeting = "good evening!"
-}
-document.getElementById("greeting").innertext=greeting
+// //both Conditions Need To Be True 
+// console.log()
+
+// //Only one condition must be true
+// console.log(10>3)||(5>3);
+
+// //If the statement is logical or not 
+
+
+
+// const hour =new Date().get hours
+// let greeting ="";
+// if (hour > 12) {
+// } greeting = "goodmorning!";
+// }else if (hour < 18) {
+// greeting = "good afternoon!";
+// } else {
+//     greeting = "good evening!"
+// }
+// document.getElementById("greeting").innertext=greeting
+6
